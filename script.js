0fb558fd4b3d2b4a23fe0a8097253d66b5de568e
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-08-24 21:34:28 -0300

    adicionando funcionalidade de inserir uma nova transaçao

--- script.js
+++ script.js
@@ -9,3 +9,95 @@ const modal = {
         modalElement.classList.remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021'
+    },
+
+    {
+        id: 4,
+        description: 'Agua',
+        amount: -30000,
+        date: '23/01/2021'
+    },
+]
+
+const Transaction = {
+    income (){
+        // somar as entradas
+    },
+
+    expense (){
+        // somar as saídas
+    },
+
+    total (){
+        // entradas - saídas
+    }
+}
+
+const DOM = {
+        transactionsContainer: document.querySelector('#data-table tbody'),
+
+        addTransaction(transaction, index){
+            const tr = document.createElement('tr')
+            tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+            DOM.transactionsContainer.appendChild(tr)
+        },
+
+        innerHTMLTransaction(transaction){
+            const CSSclass = transaction.amount > 0 ? "income" : "expense"
+            
+            const amount = Utils.formatCurrency(transaction.amount)
+
+            const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+            `
+            return html
+        }
+}
+
+const Utils = {
+    formatCurrency (value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+        
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
